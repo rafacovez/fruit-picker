0628ea4decb9fcef30705bc215e78d1ee208bc9a
Adán Estévez <adanescollante@gmail.com>
2023-03-16 18:41:48 -0300

    coded postOrder function

--- fruit-picker.js
+++ fruit-picker.js
@@ -42,5 +42,5 @@ export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
  * @return void
  */
 export function postOrder(variety, quantity) {
-  //implement the postOrder function to create a query and order
+  orderFromGrocer({variety, quantity}, onSuccess, onError);
 }
